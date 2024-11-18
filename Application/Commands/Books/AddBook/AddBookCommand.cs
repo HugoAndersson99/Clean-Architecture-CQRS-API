d09f2fe94d8f044721bf9046a54df5b085c1dac5
Hugo.csharp@hotmail.com <Hugo.csharp@hotmail.com>
2024-11-18 16:46:37 +0100

    Added a bit of CQRS

---
+++ Application/Commands/Books/AddBook/AddBookCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Application.Commands.Books.AddBook
+{
+    public class AddBookCommand : IRequest<Guid>
+    {
+        public string Title { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
--- Domain/Book.cs
+++ Domain/Book.cs
@@ -2,14 +2,7 @@
 {
     public class Book
     {
-        public Book(Guid id, string title, string description, Guid authorId, Author author)
-        {
-            Id = id;
-            Title = title;
-            Description = description;
-            AuthorId = authorId;
-            Author = author;
-        }
+        
 
         public Guid Id { get; set; }
         public string Title { get; set; }
